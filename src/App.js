3787ee399e305cbf0784f19546b44d2d53f52648
Art Iffy <iffyart1234@gmail.com>
2022-01-15 00:07:46 +0800

    feat(App): create todo list component use hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-function App() {
+import { useState } from 'react';
+
+const TodoList = ({ todoList }) => {
+  return (
+    <ul>
+      {todoList.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
+  );
+};
+
+const App = () => {
+  const [text, setText] = useState('');
+  const [todoList, setTodoList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!text.trim()) {
+      setText('');
+      return;
+    }
+
+    setTodoList([...todoList, text]);
+    setText('');
+  };
+
   return (
     <div>
-      <h1>Home Page</h1>
+      <h3>TODO</h3>
+      <TodoList todoList={todoList} />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='new-todo'>What needs to be done?</label>
+        <input
+          id='new-todo'
+          onChange={(e) => setText(e.target.value)}
+          value={text}
+        />
+        <button>Add</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
