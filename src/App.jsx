d7f915bf095245e6822079dd6ba4f962ff5e7a9d
Art Iffy <iffyart1234@gmail.com>
2022-01-22 17:40:43 +0800

    feat(project): create basic sass example

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 const App = () => {
   return (
     <div>
-      <h3>Main branch</h3>
+      <h1>Main branch h1</h1>
+      <h2>Main branch h2</h2>
+      <h3>Main branch h3</h3>
+      <a href='https://sass-lang.com/'>SASS 官網</a>
     </div>
   );
 };
